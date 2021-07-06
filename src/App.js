08a707e6972ac24680cfe95335a9b99ecb4d6618
山岡智裕 <>
2021-07-06 16:02:41 +0900

    apiを叩けるように変更

--- src/App.js
+++ src/App.js
@@ -9,12 +9,14 @@ function App() {
   // queryが格納される。
   const [query, setQuery] = useState('apple');
 
+  //
   useEffect(() => {
     console.log('useEffectが走りました。')
     fetch(`https://api.unsplash.com/search/photos?query=${query}&client_id=${process.env.REACT_APP_CLIENT_ID}`)
         .then(response => response.json())
         .then(data => {
             console.log(data)
+            //data結果をsetImagesに表示。
             setImages(data.results)
         })
   }, [query])
@@ -26,12 +28,16 @@ function App() {
     console.log("onSubmitが呼ばれました。")
   } 
 
+  //検索のボタンが押される
+  //queryが変更された際に、APIを叩く
+  //APIの叩いた結果をJSON(dataとして)返す。
   return (
     <div className="App">
       <div className="main">
         <form onSubmit={onSubmit}>
           <input
             type="text"
+            //onChangeで入力された内容を変更する。
             onChange={e => setText(e.target.value)}
             value={text}
           />
